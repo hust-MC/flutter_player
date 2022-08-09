39c1fbe0f9337596abb0c90c1f0c3de809751388
emercyma <emercyma@tencent.com>
2022-08-10 03:58:59 +0800

    fit

--- lib/video_view.dart
+++ lib/video_view.dart
@@ -1,26 +1,25 @@
 import 'package:fijkplayer/fijkplayer.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 import 'player.dart';
 
 class VideoView extends StatefulWidget {
   final Player player;
+  final FijkFit fit;
 
-  const VideoView(this.player);
+  const VideoView(this.player, {this.fit = FijkFit.contain});
 
   @override
   State<StatefulWidget> createState() => _VideoViewState();
 }
 
 class _VideoViewState extends State<VideoView> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: GestureDetector(
       child: Stack(children: [
-        AbsorbPointer(absorbing: true, child: FijkView(player: widget.player)),
+        AbsorbPointer(absorbing: true, child: FijkView(player: widget.player, fit: widget.fit)),
         if (widget.player.state == FijkState.paused)
           Align(
             child: Image.asset('asset/images/play.png', width: 70, height: 70),
